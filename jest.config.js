6c73d55aba91251794502c375914cad95e393f8d
Michael Lahr <michael.lahr@gmail.com>
2025-10-23 17:44:11 +0800

    set test timeout to 60 secs

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    testTimeout: 10000,
+    testTimeout: 60000,
     roots: ['<rootDir>/src'],
     testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
     collectCoverageFrom: [
